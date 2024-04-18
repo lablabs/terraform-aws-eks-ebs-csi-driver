1e55710157cf2772c1ff2e3ae9a73bd66b0ba81d
Balsir <jubo.stehlik@gmail.com>
2024-04-18 12:03:49 +0200

    ci: Satisfy pre-commit

---
+++ examples/basic/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.35.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+  }
+}
