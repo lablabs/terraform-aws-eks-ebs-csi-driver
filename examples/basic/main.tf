be9e781b6f4ce882991712475fa1bf1b45c0611d
Attila Sovak <attila.sovak@gmail.com>
2025-04-29 13:16:53 +0200

    feat(ebs-csi-driver): add fixed files based on universal module

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,4 +1,4 @@
-module "ebs_csi_disabled" {
+module "addon_installation_disabled" {
   source = "../../"
 
   enabled = false
@@ -7,27 +7,7 @@ module "ebs_csi_disabled" {
   cluster_identity_oidc_issuer_arn = module.eks_cluster.eks_cluster_identity_oidc_issuer_arn
 }
 
-module "ebs_without_irsa_role" {
-  source = "../../"
-
-  enabled = true
-
-  irsa_role_create                 = false
-  cluster_identity_oidc_issuer     = module.eks_cluster.eks_cluster_identity_oidc_issuer
-  cluster_identity_oidc_issuer_arn = module.eks_cluster.eks_cluster_identity_oidc_issuer_arn
-}
-
-module "ebs_without_irsa_policy" {
-  source = "../../"
-
-  enabled = true
-
-  irsa_policy_enabled              = false
-  cluster_identity_oidc_issuer     = module.eks_cluster.eks_cluster_identity_oidc_issuer
-  cluster_identity_oidc_issuer_arn = module.eks_cluster.eks_cluster_identity_oidc_issuer_arn
-}
-
-module "ebs_csi_helm" {
+module "addon_installation_helm" {
   source = "../../"
 
   enabled           = true
@@ -37,20 +17,13 @@ module "ebs_csi_helm" {
   cluster_identity_oidc_issuer     = module.eks_cluster.eks_cluster_identity_oidc_issuer
   cluster_identity_oidc_issuer_arn = module.eks_cluster.eks_cluster_identity_oidc_issuer_arn
 
-  helm_release_name = "aws-ebs-csi-helm"
-  namespace         = "aws-ebs-csi-helm"
-
   values = yamlencode({
-    "podLabels" : {
-      "app" : "aws-ebs-csi-helm"
-    }
+    # insert sample values here
   })
-
-  helm_timeout = 240
-  helm_wait    = true
 }
 
-module "ebs_csi_argo_kubernetes" {
+# Please, see README.md and Argo Kubernetes deployment method for implications of using Kubernetes installation method
+module "addon_installation_argo_kubernetes" {
   source = "../../"
 
   enabled           = true
@@ -60,16 +33,17 @@ module "ebs_csi_argo_kubernetes" {
   cluster_identity_oidc_issuer     = module.eks_cluster.eks_cluster_identity_oidc_issuer
   cluster_identity_oidc_issuer_arn = module.eks_cluster.eks_cluster_identity_oidc_issuer_arn
 
-  helm_release_name = "aws-ebs-csi-argo-kubernetes"
-  namespace         = "aws-ebs-csi-argo-kubernetes"
+  values = yamlencode({
+    # insert sample values here
+  })
 
   argo_sync_policy = {
-    "automated" : {}
-    "syncOptions" = ["CreateNamespace=true"]
+    automated   = {}
+    syncOptions = ["CreateNamespace=true"]
   }
 }
 
-module "ebs_csi_argo_helm" {
+module "addon_installation_argo_helm" {
   source = "../../"
 
   enabled           = true
@@ -79,12 +53,8 @@ module "ebs_csi_argo_helm" {
   cluster_identity_oidc_issuer     = module.eks_cluster.eks_cluster_identity_oidc_issuer
   cluster_identity_oidc_issuer_arn = module.eks_cluster.eks_cluster_identity_oidc_issuer_arn
 
-  helm_release_name = "aws-ebs-csi-argo-helm"
-  namespace         = "aws-ebs-csi-argo-helm"
-
-  argo_namespace = "argo"
   argo_sync_policy = {
-    "automated" : {}
-    "syncOptions" = ["CreateNamespace=true"]
+    automated   = {}
+    syncOptions = ["CreateNamespace=true"]
   }
 }
--- iam.tf
+++ iam.tf
@@ -1,9 +1,5 @@
-locals {
-  irsa_role_create = var.enabled && var.service_account_create && var.irsa_role_create
-}
-
 data "aws_iam_policy_document" "this" {
-  count = local.irsa_role_create && var.irsa_policy_enabled ? 1 : 0
+  count = var.enabled && var.irsa_policy == null ? 1 : 0
 
   #checkov:skip=CKV_AWS_111 there is correct condition for existing Tags
   #checkov:skip=CKV_AWS_356
@@ -152,59 +148,4 @@ data "aws_iam_policy_document" "this" {
       values   = ["true"]
     }
   }
-}
-
-resource "aws_iam_policy" "this" {
-  count = local.irsa_role_create && var.irsa_policy_enabled ? 1 : 0
-
-  name        = "${var.irsa_role_name_prefix}-${var.helm_chart_name}"
-  path        = "/"
-  description = "Policy for EBS CSI driver"
-  policy      = data.aws_iam_policy_document.this[0].json
-
-  tags = var.irsa_tags
-}
-
-data "aws_iam_policy_document" "this_irsa" {
-  count = local.irsa_role_create ? 1 : 0
-
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-
-    principals {
-      type        = "Federated"
-      identifiers = [var.cluster_identity_oidc_issuer_arn]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(var.cluster_identity_oidc_issuer, "https://", "")}:sub"
-
-      values = [
-        "system:serviceaccount:${var.namespace}:${var.service_account_name}",
-      ]
-    }
-
-    effect = "Allow"
-  }
-}
-
-resource "aws_iam_role" "this" {
-  count              = local.irsa_role_create ? 1 : 0
-  name               = "${var.irsa_role_name_prefix}-${var.helm_chart_name}"
-  assume_role_policy = data.aws_iam_policy_document.this_irsa[0].json
-  tags               = var.irsa_tags
-}
-
-resource "aws_iam_role_policy_attachment" "this" {
-  count      = local.irsa_role_create && var.irsa_policy_enabled ? 1 : 0
-  role       = aws_iam_role.this[0].name
-  policy_arn = aws_iam_policy.this[0].arn
-}
-
-resource "aws_iam_role_policy_attachment" "this_additional" {
-  for_each = local.irsa_role_create ? var.irsa_additional_policies : {}
-
-  role       = aws_iam_role.this[0].name
-  policy_arn = each.value
 }
