14e53ad2960c4e53284f0e2c8ea1f3dfda0ed659
Attila Sovak <attila.sovak@gmail.com>
2025-05-14 13:46:01 +0200

    fix(ebs-csi-driver): change role name to match with helm_chart_name as per old module

--- main.tf
+++ main.tf
@@ -20,6 +20,7 @@ locals {
   addon_irsa = {
     (local.addon.name) = {
       irsa_role_name_prefix = var.irsa_role_name_prefix != null ? var.irsa_role_name_prefix : local.addon.name
+      irsa_role_name        = var.irsa_role_name != null ? var.irsa_role_name : local.addon.helm_chart_name
       irsa_policy_enabled   = local.irsa_policy_enabled
       irsa_policy           = var.irsa_policy != null ? var.irsa_policy : data.aws_iam_policy.this[0].policy
     }
